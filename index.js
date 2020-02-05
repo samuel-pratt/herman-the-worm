e2f587e5486c7028bc6986dcf9c4658ac517fb36
sjpratt19 <sjpratt19@gmail.com>
2020-02-04 19:50:21 -0800

    Fixed error from not having declared app

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const logger = require("morgan");
 
+const app = express();
+
 app.set("port", process.env.PORT || 9001);
 
 app.enable("verbose errors");
