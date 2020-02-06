3a7e2707d8df57491a91a19d7508e4720b19f001
sjpratt19 <sjpratt19@gmail.com>
2020-02-05 17:41:51 -0800

    test

--- index.js
+++ index.js
@@ -15,14 +15,28 @@ app.use(bodyParser.json());
  * TODO:
  * Create log file that stores:
  *    gameId, turn, number of other snakes, health, length of snake on /end
- * Create game loop that calls the following functions depending on health
+ * Create game loop that calls the following functions depending on health and turn
  * curl():
  *    function that when called returns next move to curl up in a circle
+ * findNearestFood():
+ *    function that calculates distances to food and returns coords of closest one
  * goTo():
  *    function that when called returns a move
  *    accepts a pair of coordinates as parameters
  *    returns the next move along an A* path towards coords
  */
+
+const curl = data => {
+  const length = data.you.body.length;
+};
+
+const findNearestFood = data => {
+  const food = data.board.food;
+};
+
+const goTo = data => {
+  const self = data.you;
+};
 
 // API info at: https://docs.battlesnake.com/snake-api
 
@@ -47,7 +61,7 @@ app.post("/move", (request, response) => {
 });
 
 app.post("/end", (request, response) => {
-  // Perform cleanup, response ignored
+  // Perform cleanup and logging, response ignored
   return response;
 });
 
