9ca1da99bc922a955727898d89f980958c9c3dd4
sjpratt19 <sjpratt19@gmail.com>
2020-06-15 22:13:14 -0700

    Bug hunting

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 
-const handlePersonalization = require('./routes/personalization');
 const handleStart = require('./routes/start');
 const handleMove = require('./routes/move');
 const handleEnd = require('./routes/end');
@@ -15,10 +14,21 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 // ROUTES
-app.get('/', handlePersonalization);
+app.get('/', handleIndex);
 app.post('/start', handleStart);
 app.post('/move', handleMove);
 app.post('/end', handleEnd);
+
+function handleIndex(request, response) {
+  var battlesnakeInfo = {
+    apiversion: '1',
+    author: 'Sam Pratt',
+    color: '#f21f3a',
+    head: 'safe',
+    tail: 'default',
+  };
+  response.status(200).json(battlesnakeInfo);
+}
 
 app.listen(PORT, () =>
   console.log(`Server listening at http://127.0.0.1:${PORT}`),
