446967bbee510916074f6b0c87236b171d17caa8
sjpratt19 <sjpratt19@gmail.com>
2020-06-15 21:53:44 -0700

    Bug hunting

--- routes/move.js
+++ routes/move.js
@@ -22,13 +22,13 @@ function moveAsCoord(move, head) {
 }
 
 function coordAsMove(coord, head) {
-  if (coord === { x: head.x, y: head.y - 1 }) {
+  if (coord.x === head.x && coord.y === head.y - 1) {
     return 'up';
-  } else if (coord === { x: head.x, y: head.y + 1 }) {
+  } else if (coord.x === head.x && coord.y === head.y + 1) {
     return 'down';
-  } else if (coord === { x: head.x - 1, y: head.y }) {
+  } else if (coord.x === head.x - 1 && coord.y === head.y) {
     return 'left';
-  } else if (coord === { x: head.x + 1, y: head.y }) {
+  } else if (coord.x === head.x + 1 && coord.y === head.y) {
     return 'right';
   }
 }
