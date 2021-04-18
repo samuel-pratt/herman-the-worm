d7c4b26a75899943f9ba42973f68333f03e49a7a
sjpratt19 <sjpratt19@gmail.com>
2021-04-18 13:24:44 -0700

    Switched to different a* library

--- routes/move.js
+++ routes/move.js
@@ -8,105 +8,15 @@ const findFoodDistances = boardFunctions.findFoodDistances;
 const offBoard = boardFunctions.offBoard;
 const onSnakes = boardFunctions.onSnakes;
 
-const astar = require('../functions/pathfinding');
-
-const easystarjs = require('easystarjs');
-const easystar = new easystarjs.js();
-let food_path = [];
-let isFoodFound = false;
-let noPathFound = false;
-
-function findPath(destination, snakes, self, width, height) {
-  const snakeHead = self.body[0];
-
-  // Create empty board array
-  let board = Array(height)
-    .fill()
-    .map(() => Array(width).fill(0));
-
-  // Add other snakes to the board
-  snakes.forEach((snake) =>
-    snake.body.forEach((element) => (board[element.y][element.x] = 1)),
-  );
-
-  // Add self to board, not including head
-  const selfBody = self.body.slice(1);
-  selfBody.forEach((element) => (board[element.y][element.x] = 1));
-
-  // Find path
-  easystar.enableSync();
-  easystar.setGrid(board);
-  easystar.setAcceptableTiles([0]);
-
-  easystar.findPath(
-    snakeHead.x,
-    snakeHead.y,
-    destination.x,
-    destination.y,
-    function (path) {
-      if (path === null) {
-        noPathFound = true;
-      } else {
-        food_path = path;
-        isFoodFound = true;
-      }
-    },
-  );
-  easystar.calculate();
-}
+const astar = require('../functions/astar');
 
 module.exports = function handleMove(request, response) {
-  /*isFoodFound = false;
-  noPathFound = false;
-
-  const gameData = request.body;
-
-  console.log('MOVE_DATA:');
-  console.log(gameData);
-
-  const possibleMoves = ['up', 'down', 'left', 'right'];
-  let move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
-
-  const boardWidth = gameData.board.width;
-  const boardHeight = gameData.board.height;
-  const food = gameData.board.food;
-  const snakes = gameData.board.snakes;
-  const self = gameData.you;
-
-  const head = self.body[0];
-  const neck = self.body[1];
-
-  const sortedFood = findFoodDistances(food, self.body[0]);
-
-  findPath(sortedFood[0].location, snakes, self, boardWidth, boardHeight);
-
-  if (noPathFound === true) {
-    for (const move of moves) {
-      const coord = moveAsCoord(move, head);
-      if (
-        !offBoard(gameData, coord) &&
-        !coordEqual(coord, neck) &&
-        !onSnakes(gameData, coord)
-      ) {
-        response.status(200).send({
-          move: move,
-        });
-      }
-    }
-  } else {
-    response.status(200).send({
-      move: coordAsMove(food_path[1], self.body[0]),
-    });
-  }*/
   const gameData = request.body;
   const width = gameData.board.width;
   const height = gameData.board.height;
   const food = gameData.board.food;
   const snakes = gameData.board.snakes;
   const self = gameData.you;
-
-  const head = self.body[0];
-  const neck = self.body[1];
 
   const sortedFood = findFoodDistances(food, self.body[0]);
 
@@ -133,16 +43,7 @@ module.exports = function handleMove(request, response) {
 
   var result = astar.astar.search(graph, start, end);
 
-  console.log('Snake Head: ');
-  console.log(snakeHead);
-  console.log('Food: ');
-  console.log(sortedFood[0].location);
-  console.log('Result: ');
-  console.log(result);
-
   var move = coordAsMove({ x: result[0].x, y: result[0].y }, snakeHead);
-
-  console.log(move);
 
   response.status(200).send({
     move: move,
