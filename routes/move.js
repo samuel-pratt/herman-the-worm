79ba111676f18e609747e311ddce933ff9371091
sjpratt19 <sjpratt19@gmail.com>
2021-04-19 21:20:25 -0700

    Switched to different a* library

--- routes/move.js
+++ routes/move.js
@@ -12,15 +12,15 @@ const astar = require('../functions/astar');
 
 module.exports = function handleMove(request, response) {
   const gameData = request.body;
+
   const width = gameData.board.width;
   const height = gameData.board.height;
   const food = gameData.board.food;
   const snakes = gameData.board.snakes;
   const self = gameData.you;
+  const snakeHead = self.body[0];
 
   const sortedFood = findFoodDistances(food, self.body[0]);
-
-  const snakeHead = self.body[0];
 
   // Create empty board array
   let board = Array(height)
@@ -43,7 +43,26 @@ module.exports = function handleMove(request, response) {
 
   var result = astar.astar.search(graph, start, end);
 
-  var move = coordAsMove({ x: result[0].x, y: result[0].y }, snakeHead);
+  if (result !== []) {
+    var move = coordAsMove({ x: result[0].x, y: result[0].y }, snakeHead);
+    response.status(200).send({
+      move: move,
+    });
+  } else {
+    for (let i = selfBody.length - 1; i > 0; i--) {
+      start = graph.grid[snakeHead.x][snakeHead.y];
+      end = graph.grid[selfBody[i].x][selfBody[i].y];
+
+      result = astar.astar.search(graph, start, end);
+
+      if (result !== []) {
+        var move = coordAsMove({ x: result[0].x, y: result[0].y }, snakeHead);
+        response.status(200).send({
+          move: move,
+        });
+      }
+    }
+  }
 
   response.status(200).send({
     move: move,
